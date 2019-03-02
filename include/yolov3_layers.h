4b9a5197b7f2b8992e18c9a4865cf9deb3428fac
Vladimir Lazic <vladimir.lazic@rt-rk.com>
2019-03-02 15:09:01 +0100

    conv2d

--- include/yolov3_layers.h
+++ include/yolov3_layers.h
@@ -22,55 +22,160 @@
 #define COEFF_NUM (8858739U)
 #define CHANNELS (3U)
 
-typedef struct {
-  float *biases;
-  float *bn_weights = NULL;
-  float *bn_running_mean = NULL;
-  float *bn_running_var = NULL;
-  float *conv_weight;
-  bool batch_normalization = false;
+typedef struct
+{
+    int filter_num;
+
+    float* biases;
+    float* bn_weights = NULL;
+    float* bn_running_mean = NULL;
+    float* bn_running_var = NULL;
+    float* conv_weight;
+
+    bool batch_normalization = false;
+
+    int input_width;
+    int input_height;
+
+    int kernel_width;
+    int kernel_height;
+    int kernel_depth;
+
+    int output_width;
+    int output_height;
+    int output_depth;
+
 } conv_configuration;
 
-void image_padding(float *input_buffer, float *output_buffer) {
+static void image_padding(float* input_buffer, float* output_buffer)
+{
 
-  memset(output_buffer, 0,
-         CHANNELS * (WIDTH + 2) * (HEIGHT + 2) * sizeof(float));
+    memset(output_buffer, 0, (WIDTH + 2) * (HEIGHT + 2) * sizeof(float));
 
-  for (int i = 0; i < HEIGHT; i++) {
-    memcpy(output_buffer + (i + 1) * (WIDTH + 2), input_buffer + i * WIDTH,
-           WIDTH * sizeof(float));
-  }
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        memcpy(output_buffer + (i + 1) * (WIDTH + 2), input_buffer + i * WIDTH,
+               WIDTH * sizeof(float));
+    }
 }
 
-static void batch_normalization(float *input_buffer, float *output_buffer,
+static void batch_normalization(float* input_buffer, float* output_buffer,
                                 conv_configuration cfg);
 
-static void conv2d(float *input_buffer, float *output_buffer,
-                   conv_configuration cfg);
+void conv2d(float* input_buffer, float* output_buffer, conv_configuration cfg)
+{
+    int input_width = cfg.input_width;
+    int input_height = cfg.input_height;
 
-void conv(float *input_buffer, float *output_buffer, conv_configuration cfg);
+    int new_image_width = cfg.output_width;
+    int new_image_height = cfg.output_height;
 
-void readCoeff(std::string file_path, float *coeff_buffer) {
-  FILE *file;
-  size_t nread;
+    float* layer_output_buffer =
+        new float[16 * 3 * new_image_width * new_image_height];
+    memset(layer_output_buffer, 0,
+           16 * 3 * new_image_width * new_image_height * sizeof(float));
 
-  file = fopen(file_path.c_str(), "r");
-  if (file) {
-    nread = fread(coeff_buffer, sizeof(float), COEFF_NUM * sizeof(float), file);
-    fclose(file);
-  }
+    for (int i = 0; i < cfg.filter_num; i++)
+    {
+        float kernels[3 * 3 * 3];
+        memcpy(kernels, cfg.conv_weight + i * 3 * 3 * 3,
+               3 * 3 * 3 * sizeof(float));
+
+        float output_img[3 * new_image_width * new_image_height];
+        memset(output_img, 0,
+               3 * new_image_width * new_image_height * sizeof(float));
+
+        for (int j = 0; j < 3; j++)
+        {
+            // Separate kernel accoriding to channels
+            float kernel[3 * 3];
+            memcpy(kernel, kernel + j * 3 * 3, 3 * 3 * sizeof(float));
+
+            // Sepparate channels and perform padding
+            float channel[input_width * input_height];
+            mempcpy(channel, input_buffer + j * input_width * input_height,
+                    input_width * input_height * sizeof(float));
+
+            float padded_channel[(input_width + 2) * (input_height + 2)];
+            image_padding(channel, padded_channel);
+
+            float output_channel[new_image_width * new_image_height];
+            memset(output_channel, 0,
+                   new_image_width * new_image_height * sizeof(float));
+            // Apply kernel
+
+            for (int ii = 3 / 2; ii < (input_width + 2) - 3 / 2; ++ii)
+            {
+                for (int jj = 3 / 2; jj < (input_height + 2) - 3 / 2; ++jj)
+                {
+                    float sum = 0;
+
+                    for (int x = -3 / 2; x <= 3 / 2; ++x)
+                    {
+                        for (int y = -3 / 2; y <= 3 / 2; ++y)
+                        {
+                            float data =
+                                padded_channel[(jj + y) * (input_width + 2) +
+                                               (ii + x)];
+
+                            float coeff =
+                                kernel[(y + 3 / 2) * (input_width + 2) +
+                                       (x + 3 / 2)];
+
+                            sum += data * coeff;
+                        }
+                    }
+                    output_channel[jj * new_image_width + ii] = sum;
+                }
+            }
+            // Combine the channels together
+            memcpy(output_img + j * new_image_width * new_image_height,
+                   output_channel,
+                   new_image_width * new_image_height * sizeof(float));
+        }
+
+        memcpy(layer_output_buffer + i * 3 * new_image_width * new_image_height,
+               output_img,
+               3 * new_image_width * new_image_height * sizeof(float));
+    }
+
+    if (cfg.batch_normalization)
+    {
+        // batch_normalization(layer_output_buffer, output_buffer, cfg);
+    }
+    else
+    {
+        memcpy(output_buffer, layer_output_buffer,
+               16 * 3 * new_image_width * new_image_height * sizeof(float));
+    }
 }
 
-void readImg(std::string file_path, float *input_img) {
-  FILE *file;
-  size_t nread;
+void readCoeff(std::string file_path, float* coeff_buffer)
+{
+    FILE* file;
+    size_t nread;
 
-  file = fopen(file_path.c_str(), "r");
-  if (file) {
-    nread = fread(input_img, sizeof(float),
-                  CHANNELS * WIDTH * HEIGHT * sizeof(float), file);
-    fclose(file);
-  }
+    file = fopen(file_path.c_str(), "r");
+    if (file)
+    {
+        nread =
+            fread(coeff_buffer, sizeof(float), COEFF_NUM * sizeof(float), file);
+        fclose(file);
+    }
+}
+
+void readImg(std::string file_path, float* input_img)
+{
+    FILE* file;
+    size_t nread;
+
+    file = fopen(file_path.c_str(), "r");
+    if (file)
+    {
+        nread = fread(input_img, sizeof(float),
+                      CHANNELS * WIDTH * HEIGHT * sizeof(float), file);
+        fclose(file);
+    }
 }
 
 #endif // YOLOV3_LAYERS_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,71 @@
 float coeff_buffer[COEFF_NUM];
 float input_img[CHANNELS * WIDTH * HEIGHT];
 
-int main() {
-  readCoeff(COEFF_FILENAME, coeff_buffer);
-  readImg(IN_FILENAME, input_img);
+int main()
+{
+    readCoeff(COEFF_FILENAME, coeff_buffer);
+    readImg(IN_FILENAME, input_img);
 
-  // Reading coeffs for first conv layer
-  int layer_0_filter_num = 16;
-  float biases[layer_0_filter_num];
-  float bn_weights[layer_0_filter_num];
-  float bn_running_mean[layer_0_filter_num];
-  float bn_running_var[layer_0_filter_num];
-  float conv_weight[3 * 3 * 3 * layer_0_filter_num];
+    // Reading coeffs for first conv layer
+    int layer_0_filter_num = 16;
+    float biases[layer_0_filter_num];
+    float bn_weights[layer_0_filter_num];
+    float bn_running_mean[layer_0_filter_num];
+    float bn_running_var[layer_0_filter_num];
+    float conv_weight[3 * 3 * 3 * layer_0_filter_num];
 
-  int i = 5;
-  memcpy(biases, coeff_buffer + i, layer_0_filter_num * sizeof(float));
-  i += layer_0_filter_num;
+    int i = 5;
+    memcpy(biases, coeff_buffer + i, layer_0_filter_num * sizeof(float));
+    i += layer_0_filter_num;
 
-  memcpy(bn_weights, coeff_buffer + i, layer_0_filter_num * sizeof(float));
-  i += layer_0_filter_num;
+    memcpy(bn_weights, coeff_buffer + i, layer_0_filter_num * sizeof(float));
+    i += layer_0_filter_num;
 
-  memcpy(bn_running_mean, coeff_buffer + i, layer_0_filter_num * sizeof(float));
-  i += layer_0_filter_num;
+    memcpy(bn_running_mean, coeff_buffer + i,
+           layer_0_filter_num * sizeof(float));
+    i += layer_0_filter_num;
 
-  memcpy(bn_running_var, coeff_buffer + i, layer_0_filter_num * sizeof(float));
-  i += layer_0_filter_num;
+    memcpy(bn_running_var, coeff_buffer + i,
+           layer_0_filter_num * sizeof(float));
+    i += layer_0_filter_num;
 
-  memcpy(conv_weight, coeff_buffer + i,
-         3 * 3 * 3 * layer_0_filter_num * sizeof(float));
-  i += 3 * 3 * 3 * layer_0_filter_num;
+    memcpy(conv_weight, coeff_buffer + i,
+           3 * 3 * 3 * layer_0_filter_num * sizeof(float));
+    i += 3 * 3 * 3 * layer_0_filter_num;
 
-  float padded_buffer[CHANNELS * (WIDTH + 2) * (HEIGHT + 2)];
-  image_padding(input_img, padded_buffer);
+    conv_configuration layer1;
 
-  return 0;
+    layer1.filter_num = layer_0_filter_num;
+
+    layer1.biases = biases;
+    layer1.bn_weights = bn_weights;
+    layer1.bn_running_mean = bn_running_mean;
+    layer1.bn_running_var = bn_running_var;
+    layer1.conv_weight = conv_weight;
+
+    layer1.batch_normalization = false;
+
+    layer1.input_width = WIDTH;
+    layer1.input_height = HEIGHT;
+
+    layer1.kernel_height = 3;
+    layer1.kernel_width = 3;
+    layer1.kernel_depth = 3;
+
+    layer1.output_width = (layer1.input_width + 2) - layer1.kernel_width + 1;
+    layer1.output_height = (layer1.input_height + 2) - layer1.kernel_height + 1;
+    layer1.output_depth = 16;
+
+    float* layer1_output =
+        new float[layer1.output_depth * 3 * layer1.output_width *
+                  layer1.output_height];
+
+    conv2d(input_img, layer1_output, layer1);
+
+    for (int i = 0; i < 1000; i++)
+    {
+        std::cout << layer1_output[i] << std::endl;
+    }
+
+    return 0;
 }
